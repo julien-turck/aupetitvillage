5657fd52c9aff83c4f5b0a6c3b62419ac275344e
julienturck <julien.turck.tic@gmail.com>
2024-06-20 10:45:57 +0200

    create component for liste of produits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,49 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ProduitsComponent } from './produits/produits.component';
-import { modele } from './modeles/produitsModele';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  produitsArray!: modele[]
-
-  ngOnInit(): void {
-    this.produitsArray = [
-      new modele(
-        "Asterix",
-        "family.png",
-        "Figurine d'Asterix",
-        20,
-        200,
-      ),
-      new modele(
-        "Obelix",
-        "family.png",
-        "Figurine d'Obelix",
-        18,
-        56,
-      ),
-      new modele(
-        "Panoramix",
-        "family.png",
-        "Figurine de Panoramix",
-        23,
-        130,
-      ),
-      new modele(
-        "Cesar",
-        "family.png",
-        "Figurine de Cesar",
-        25,
-        0,
-      ),
-    ]
-    this.produitsArray[0].setStock(20);
-    this.produitsArray[1].setStock(6);
-    this.produitsArray[2].setStock(0);
-    this.produitsArray[3].setStock(43);
-  }
-}
+export class AppComponent {}
