4c65dc895d43651b32329d2bdffafcdb3b1f2fb1
julienturck <julien.turck.tic@gmail.com>
2024-09-16 11:06:55 +0200

    write readme file + check points of security

--- src/app/sort-by-price.pipe.ts
+++ src/app/sort-by-price.pipe.ts
@@ -1,16 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'sortByPrice'
+  name: 'sortByPrice',
 })
 export class SortByPricePipe implements PipeTransform {
-
   transform(produits: any[]): any[] {
-    return produits.sort((a:any,b:any) => {
-      if(a.prix < b.prix) { return 1;}
-      else if (a.prix > b.prix) {return -1;}
-      else return 0;
-    })
+    return produits.sort((a: any, b: any) => {
+      if (a.prix < b.prix) {
+        return 1;
+      } else if (a.prix > b.prix) {
+        return -1;
+      } else return 0;
+    });
   }
-
 }
